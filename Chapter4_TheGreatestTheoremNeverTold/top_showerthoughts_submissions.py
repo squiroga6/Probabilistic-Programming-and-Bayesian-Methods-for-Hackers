523903dbc8b4d263809c0ef88782039850d1f0b0
squiroga <squirog@gmail.com>
2021-03-06 17:14:30 -0700

    reddit log in info

--- Chapter4_TheGreatestTheoremNeverTold/top_showerthoughts_submissions.py
+++ Chapter4_TheGreatestTheoremNeverTold/top_showerthoughts_submissions.py
@@ -4,12 +4,22 @@ import numpy as np
 from IPython.core.display import Image
 
 import praw
+import json
 
+with open("../config.json", "r") as read_file:
+    data = json.load(read_file)
 
-reddit = praw.Reddit("BayesianMethodsForHackers")
-subreddit  = reddit.get_subreddit("showerthoughts")
+# reddit = praw.Reddit("BayesianMethodsForHackers")
 
-top_submissions = subreddit.get_top(limit=100)
+reddit = praw.Reddit(client_id=data['user_id'],
+                     client_secret=data['secret'],
+                     user_agent="Bayes methods for hackers course",
+                     username=data['username'],
+                     password=data['password'])
+
+subreddit  = reddit.subreddit("showerthoughts")
+
+top_submissions = subreddit.top(limit=100)
 
 n_sub = int( sys.argv[1] ) if sys.argv[1] else 1
 
@@ -24,13 +34,24 @@ upvotes = []
 downvotes = []
 contents = []
 
+# for sub in top_submissions:
+#     try:
+#         ratio = reddit.get_submission(sub.permalink).upvote_ratio
+#         ups = int(round((ratio*sub.score)/(2*ratio - 1)) if ratio != 0.5 else round(sub.score/2))
+#         upvotes.append(ups)
+#         downvotes.append(ups - sub.score)
+#         contents.append(sub.title)
+#     except Exception as e:
+#         continue
+
 for sub in top_submissions:
     try:
-        ratio = reddit.get_submission(sub.permalink).upvote_ratio
+        ratio = sub.upvote_ratio
         ups = int(round((ratio*sub.score)/(2*ratio - 1)) if ratio != 0.5 else round(sub.score/2))
-        upvotes.append(ups)
+        upvotes.append(sub.ups)
         downvotes.append(ups - sub.score)
         contents.append(sub.title)
     except Exception as e:
-        continue
+        raise e
+        
 votes = np.array( [ upvotes, downvotes] ).T
